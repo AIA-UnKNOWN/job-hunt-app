a93d81c9180ef30c65b858a12226cd762c16eefa
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-23 21:28:02 +0800

    setup database

---
+++ database/migrations/2022_04_23_131222_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longtext('description');
+            $table->integer('min_salary')->nullable();
+            $table->integer('max_salary')->nullable();
+            $table->enum('status', ['active', 'closed']);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}

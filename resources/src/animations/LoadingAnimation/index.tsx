74585b4f3466052c98d362fa3a3935ff7662dcac
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-26 01:41:19 +0800

    refactor loading animation UI

--- resources/src/animations/LoadingAnimation/index.tsx
+++ resources/src/animations/LoadingAnimation/index.tsx
@@ -1,14 +1,10 @@
 import './style.css';
 import { FC } from 'react';
 
-interface LoadingAnimationProps {
-  size: string
-}
-
-const LoadingAnimation: FC<LoadingAnimationProps> = ({ size }) => {
+const LoadingAnimation: FC = () => {
   return (
     <div className="flex flex-col flex-1 justify-center items-center mb-4">
-      <div className={`fetch-loading border-[7px] border-black border-t-8 border-t-white h-[40px] w-[40px] rounded-full shadow-md scale-${size}`}></div>
+      <div className="fetch-loading border-[7px] border-black border-t-8 border-t-white h-[40px] w-[40px] rounded-full shadow-md"></div>
     </div>
   );
 }

d1de6792c981200c351061145131f122b87953f2
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-26 05:52:58 +0800

    created register as applicant UI

---
+++ resources/src/pages/Auth/RegisterAsApplicant/hook.ts
@@ -0,0 +1,53 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { AUTH_SCREENS, setCurrentAuth } from '@reducers/authSlice';
+
+const useRegisterAsApplicant = () => {
+  const dispatch = useDispatch();
+  const [submitButtonText, setSubmitButtonText] = useState('Submit');
+  
+  const onRegister = async data => {
+    setSubmitButtonText('Submitting...');
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          first_name: data.firstname,
+          last_name: data.lastname,
+          email: data.email,
+          password: data.password,
+          password_confirmation: data.confirmPassword,
+          role: 'applicant'
+        })
+      });
+      if (!response.ok) {
+        toast.error('Email already taken', {
+          position: "top-center",
+          autoClose: 1000,
+        });
+        return;
+      }
+      toast('Submitted!', {
+        position: "top-center",
+        autoClose: 1000,
+      });
+      setTimeout(() => {
+        dispatch(setCurrentAuth(AUTH_SCREENS[0]));
+      }, 1000);
+    } catch(error) {
+      console.error(error);
+    } finally {
+      setSubmitButtonText('Submit');
+    }
+  }
+
+  return { onRegister, submitButtonText };
+}
+
+export default useRegisterAsApplicant;
--- resources/src/pages/Auth/RegisterAsApplicant/index.tsx
+++ resources/src/pages/Auth/RegisterAsApplicant/index.tsx
@@ -1,7 +1,33 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { setCurrentAuth } from '@reducers/authSlice';
+import { AUTH_SCREENS } from '@reducers/authSlice';
+import BackButton from '@common/BackButton';
+import RegisterForm from '@common/RegisterForm';
+import useRegisterAsApplicant from './hook';
+
 const RegisterAsApplicant = () => {
+  const dispatch = useDispatch();
+  const { onRegister, submitButtonText } = useRegisterAsApplicant();
+
   return (
-    <div>
-      <h1>Register as applicant</h1>
+    <div className="relative flex min-h-screen flex-col justify-center items-center">
+      <ToastContainer />
+      <div className="absolute top-[10px] left-[10px]">
+        <BackButton
+          onClick={() => dispatch(setCurrentAuth(AUTH_SCREENS[0]))}
+        />
+      </div>
+      <div>
+        <div className="mb-5">
+          <p className="text-center text-[25px]">Register as applicant</p>
+        </div>
+        <RegisterForm
+          onSubmit={onRegister}
+          submitButtonText={submitButtonText}
+        />
+      </div>
     </div>
   );
 }
